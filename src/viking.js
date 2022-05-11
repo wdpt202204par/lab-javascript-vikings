6b2d1d12b4b3e6716866e0a5f813904eb0681a7f
Jossuha <Jossuha@hotmail.fr>
2022-05-11 19:03:02 +0200

    V1

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,31 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength){
+    this.health = Number(health)
+    this.strength = Number(strength)
+  }
+
+  attack(){
+    return this.strength
+  }
+
+  receiveDamage(damage){
+    this.health -= damage
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+
+  constructor(name , health, strength){
+    super(health, strength)
+    this.name = name
+  }
+
+  
+
+
+}
 
 // Saxon
 class Saxon {}
